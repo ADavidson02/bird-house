90f046d411c4aa36bc1e73a0928789ddc28c0ae1
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-03-24 14:26:14 -0600

    Add new App component folder

---
+++ src/Components/App/App.js
@@ -0,0 +1,33 @@
+
+import React, { useState } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Overview from '../Overview/Overview'
+import Map from '../Map/Map'
+import Gallery from '../Gallery/Gallery'
+import Rates from '../Rates/Rates'
+import Availability from '../Availability/Avilability'
+import Reviews from '../Reviews/Reviews'
+import Contact from '../Contact/Contact'
+import Book from '../Book/Book'
+
+import './App.css';
+
+const App = () => {
+  return (
+    <main>
+      <Switch>
+        <Route exact path='/' />
+        <Route exact path='/overview' component={Overview} />
+        <Route exact path='/map' component={Map} />
+        <Route exact path='/gallery' component={Gallery} />
+        <Route exact path='/rates' component={Rates} />
+        <Route exact path='/avilability' component={Availability} />
+        <Route exact path='/reviews' component={Reviews}/>
+        <Route exact path='/contact' component={Contact}/>
+        <Route exact path='/book' component={Book}/>
+      </Switch>
+    </main>
+  );
+}
+
+export default App;
