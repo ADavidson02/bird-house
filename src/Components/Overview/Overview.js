2a80586949c2f43c9006a71a448b57fdc2442b7a
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-04-05 17:06:10 -0600

    Add points of interest

--- src/Components/Overview/Overview.js
+++ src/Components/Overview/Overview.js
@@ -1,9 +1,47 @@
 import React from 'react'
+import { Grid, makeStyles, ThemeProvider, Typography } from '@material-ui/core';
+import PeopleOutlinedIcon from '@material-ui/icons/PeopleOutlined';
+import { theme } from '../../themes/themes';
+import KingBedOutlinedIcon from '@material-ui/icons/KingBedOutlined';
+import EmojiFoodBeverageOutlinedIcon from '@material-ui/icons/EmojiFoodBeverageOutlined';
+import BathtubOutlinedIcon from '@material-ui/icons/BathtubOutlined';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  grid: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignContent: 'start',
+  }
+}))
 
 const Overview = () => {
+  const classes = useStyles();
+
   return (
-    <h2>Overview</h2>
+    <ThemeProvider theme={theme}>
+      <Grid container>
+
+      </Grid>
+      <div>
+        <Typography variant='h4'>
+          Loft
+        </Typography>
+        <Grid item className={classes.grid}>
+        <PeopleOutlinedIcon />
+         4 Guests
+          <KingBedOutlinedIcon />
+         2 beds
+        <BathtubOutlinedIcon />
+        1 bathroom
+        <EmojiFoodBeverageOutlinedIcon />
+        Full kitchen
+        </Grid>
+      </div>
+    </ThemeProvider>
+    
   )
 }
 
