fe234f40b370256a2e3029b2a805a58199647b31
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-04-05 11:38:16 -0600

    Add book handle click

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,22 +1,18 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Grid, makeStyles } from '@material-ui/core';
-import Button from '@material-ui/core/Button'
-import{ Paper, Container } from '@material-ui/core';
-import Typography from '@material-ui/core/Typography';
-import imageOne from '../../assets/bird-house-1.webp'
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import { red } from '@material-ui/core/colors';
+// import { Link } from 'react-router-dom';
+import { Grid, makeStyles, ThemeProvider } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import { Paper } from '@material-ui/core';
+import { theme } from '../../themes/themes';
+// import Typography from '@material-ui/core/Typography';
+// import imageOne from '../../assets/bird-house-1.webp'
+// import GridList from '@material-ui/core/GridList';
+// import GridListTile from '@material-ui/core/GridListTile';
 import {
   MuiPickersUtilsProvider,
-  KeyboardTimePicker,
   KeyboardDatePicker,
 } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
-
-
-
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -36,100 +32,95 @@ const useStyles = makeStyles((theme) => ({
     overflow: 'hidden',
     backgroundColor: theme.palette.background.paper,
   },
-  textField: {
-    
-
-  }
+  textField: {},
 }));
 
 const Home = (props) => {
   let { history } = props;
-  const classes = useStyles()
-  const [value, setValue] = useState('')
-  const [selectedDate, setSelectedDate] = useState(new Date(''))
-  const [arrivalDate, setArrivalDate] = useState(null)
-  const [departureDate, setDepartureDate] = useState(null)
-  
-  const handleDateChange = (date) => {
-    setSelectedDate(date)
-  }
+  const classes = useStyles();
+  const [arrivalDate, setArrivalDate] = useState(null);
+  const [departureDate, setDepartureDate] = useState(null);
 
-  const handleArrivalChange  = (date) => {
-    setArrivalDate(date)
-    console.log(arrivalDate)
-  }
-  const handleDepartureChange = (date) => {
-    setDepartureDate(date)
-  }
+  const handleArrivalChange = (arrivalDate) => {
+    setArrivalDate(arrivalDate);
+  };
+  const handleDepartureChange = (departureDate) => {
+    setDepartureDate(departureDate);
+  };
 
-  const handleBook = (arrivalDate, departureDate) => {
-  }
+  const handleBook = (aDate, dDate) => {
+    console.log(aDate);
+    console.log(dDate);
+    history.push('/book');
+  };
 
   return (
-    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <Grid container spacing={6}>
-        <Grid item xs={12}>
-          <Paper>
-            <KeyboardDatePicker
-              disableToolbar
-              variant='outlined'
-              format='MM/dd/yyyy'
-              margin='normal'
-              id='arrival-date'
-              label='Arrival'
-              value={arrivalDate}
-              onChange={handleArrivalChange}
-              KeyboardButtonProps={{
-                'aria-label': 'change date',
-              }}
-            />
-            <KeyboardDatePicker
-              disableToolbar
-              variant='outlined'
-              format='MM/dd/yyyy'
-              margin='normal'
-              id='arrival-date'
-              label='Departure'
-              value={departureDate}
-              onChange={handleDepartureChange}
-              KeyboardButtonProps={{
-                'aria-label': 'change date',
-              }}
-            />
-            <Button onClick={() => handleBook}>Book Now</Button>
-          </Paper>
+    <ThemeProvider theme={theme}>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <Grid container spacing={6}>
+          <Grid item xs={12}>
+            <Paper>
+              <KeyboardDatePicker
+                disableToolbar
+                variant='outlined'
+                format='MM/dd/yyyy'
+                margin='normal'
+                id='arrival-date'
+                label='Arrival'
+                value={arrivalDate}
+                onChange={handleArrivalChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change arrival date',
+                }}
+              />
+              <KeyboardDatePicker
+                disableToolbar
+                variant='outlined'
+                format='MM/dd/yyyy'
+                margin='normal'
+                id='departure-date'
+                label='Departure'
+                value={departureDate}
+                onChange={handleDepartureChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change depature date',
+                }}
+              />
+              <Button onClick={(e) => handleBook(arrivalDate, departureDate)}>Book Now</Button>
+            </Paper>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            style={{ outline: 'red 3px' }}
+            className={classes.column}
+          >
+            <Paper elevation={0} className={classes.paper}>
+              Picture
+            </Paper>
+            <Paper elevation={0} className={classes.paper}>
+              Description
+            </Paper>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            style={{ outline: 'red 3px' }}
+            className={classes.column}
+          >
+            <Paper elevation={0} className={classes.paper}>
+              Picture
+            </Paper>
+            <Paper elevation={0} className={classes.paper}>
+              Description
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper>6</Paper>
+          </Grid>
         </Grid>
-        <Grid
-          item
-          xs={12}
-          style={{ outline: 'red 3px' }}
-          className={classes.column}
-        >
-          <Paper elevation={0} className={classes.paper}>
-            Picture
-          </Paper>
-          <Paper elevation={0} className={classes.paper}>
-            Description
-          </Paper>
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          style={{ outline: 'red 3px' }}
-          className={classes.column}
-        >
-          <Paper elevation={0} className={classes.paper}>
-            Picture
-          </Paper>
-          <Paper elevation={0} className={classes.paper}>
-            Description
-          </Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper>6</Paper>
-        </Grid>
-      </Grid>
-    </MuiPickersUtilsProvider>
+      </MuiPickersUtilsProvider>
+    </ThemeProvider>
   );
 };
 
