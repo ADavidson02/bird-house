2bf37fc9a08535dd424d07b386a4287c1c6e97df
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-04-05 17:24:43 -0600

    Add top blurb

--- src/Components/Overview/Overview.js
+++ src/Components/Overview/Overview.js
@@ -5,6 +5,8 @@ import { theme } from '../../themes/themes';
 import KingBedOutlinedIcon from '@material-ui/icons/KingBedOutlined';
 import EmojiFoodBeverageOutlinedIcon from '@material-ui/icons/EmojiFoodBeverageOutlined';
 import BathtubOutlinedIcon from '@material-ui/icons/BathtubOutlined';
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
+import FingerprintIcon from '@material-ui/icons/Fingerprint';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,27 +24,35 @@ const Overview = () => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Grid container>
-
-      </Grid>
+      <Grid container></Grid>
       <div>
-        <Typography variant='h4'>
-          Loft
-        </Typography>
+        <Typography variant='h4'>Loft</Typography>
         <Grid item className={classes.grid}>
-        <PeopleOutlinedIcon />
-         4 Guests
+          <HomeOutlinedIcon />
+          Entire House
+          <PeopleOutlinedIcon />
+          4 Guests
           <KingBedOutlinedIcon />
-         2 beds
-        <BathtubOutlinedIcon />
-        1 bathroom
-        <EmojiFoodBeverageOutlinedIcon />
-        Full kitchen
+          2 beds
+          <FingerprintIcon />
+          Self check-in
+          <EmojiFoodBeverageOutlinedIcon />
+          Full kitchen
         </Grid>
+        <p>
+          The Bird House is a completely private studio with everything you
+          need! With no shared spaces or walls, a large private deck with
+          beautiful scenery, this is the perfect place to unwind after a day of
+          exploring! Or cuddle up with the elegant electric fireplace, and watch
+          your favorite streaming services on a 50" TV and just stay in and
+          relax. The modern kitchen makes cooking simple & convenient and the
+          stunning bathroom with 2 shower heads will leave you refreshed, never
+          wanting to leave!
+        </p>
+
       </div>
     </ThemeProvider>
-    
-  )
+  );
 }
 
 export default Overview;
