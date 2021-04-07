0f4f0e417691c1e17bca0543eab104e0f2d3c21f
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-04-07 14:06:21 -0600

    Add link to airbnb

--- src/Components/Book/Book.js
+++ src/Components/Book/Book.js
@@ -2,7 +2,30 @@ import React from 'react';
 
 
 const Book = () => {
-  return <h2>Book</h2>;
-};
+  return (
+    <div>
+      <h2>Book</h2>
+      <div
+        class='airbnb-embed-frame'
+        data-id='35343574'
+        data-view='home'
+        style={{ width: '450px', height: '300px', margin: 'auto' }}
+      >
+        <a href='https://www.airbnb.com/rooms/35343574?guests=1&amp;adults=1&amp;s=66&amp;unique_share_id=8153f2bf-3c80-424c-9558-406ad9f49233&amp;source=embed_widget'>
+          View On Airbnb
+        </a>
+        <a
+          href='https://www.airbnb.com/rooms/35343574?guests=1&amp;adults=1&amp;s=66&amp;unique_share_id=8153f2bf-3c80-424c-9558-406ad9f49233&amp;source=embed_widget'
+          rel='nofollow'
+        >
+          The Bird House of Frederick-Open Space &amp; Lake View
+        </a>
+        <script
+          async=''
+          src='https://www.airbnb.com/calendar/ical/35343574.ics?s=549863931680e50cd7c73a638bba385a'
+        ></script>
+      </div>
+    </div>
+  );}
 
 export default Book;
