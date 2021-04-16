51d69affee45c2d056341d4495794d96bc64e01d
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-04-16 12:02:10 -0600

    Add neighborhood description

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,8 +1,26 @@
+import { makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
+import { theme } from '../../themes/themes';
+
+const useStyles = makeStyles((theme) => {
+
+})
 
 
 const Map = () => {
-  return <h2>Map</h2>;
+  return (
+    <div>
+      <Typography variant='h3'>Location</Typography>
+      <div>
+        <Typography variant='h5'>neighborhood description:</Typography>
+        <p>
+          The neighborhood is very quite and with miles of trail systems,
+          including walking to the lake, accessible from the backyard it is a
+          beautiful place to stay!
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Map;
