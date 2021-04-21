414984618e55fec36349940567a74f2d16a85223
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-04-21 16:39:10 -0600

    Add first review

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -52,7 +52,32 @@ const Reviews = () => {
           Reviews
         </Typography>
       </header>
-      
+      <main>
+        <Grid container direction='row' className={classes.root} spacing={2}>
+          <Grid item xs={12} s={6} md={4} l={3}>
+            <Card className={classes.cardRoot}>
+              <CardContent>
+                <Typography variant='h5' component='h2'>
+                  Arun
+                </Typography>
+                <StarIcon style={{ color: '#01579b' }}></StarIcon>
+                <StarIcon style={{ color: '#01579b' }}></StarIcon>
+                <StarIcon style={{ color: '#01579b' }}></StarIcon>
+                <StarIcon style={{ color: '#01579b' }}></StarIcon>
+                <StarIcon style={{ color: '#01579b' }}></StarIcon>
+                <Typography variant='body2' component='p'>
+                  This property is one of the best Airbnb’s I have stayed in.
+                  Immaculate, spotless, perfection are some of the words which
+                  come to mind to describe this property. It is located in a
+                  great neighborhood and has an awesome deck. The property has
+                  all the amenities you need and more. Last but not least, Micah
+                  and his wife are such super friendly people and amazing hosts.
+                  Thoroughly enjoyed my stay for 2 weeks and wish it was longer.
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+      </main>
     </div>
   );
 };
