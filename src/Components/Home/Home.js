7c4d52248e59cede4737020a39d85a1c29768020
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-04-21 10:28:50 -0600

    Switch from grid to gridList

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,14 +8,19 @@ import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
 import { DatePicker } from '@material-ui/pickers';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around'
   },
   gridList: {
-    width: 500,
-    height: 450,
+    width: '600',
+    height: '450',
     // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
     transform: 'translateZ(0)',
   },
@@ -35,18 +40,10 @@ const useStyles = makeStyles((theme) => ({
   bar: {
     marginTop: '1em',
     marginLeft: '1em',
-    outline: '3px solid red',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     zIndex: '1',
-  },
-  image: {
-    width: '30em',
-    height: '20em',
-    borderRadius: '.3125em',
-    display: 'block',
-    position: 'relative',
   },
   button: {
     color: 'white',
@@ -92,87 +89,145 @@ const Home = (props) => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Grid container className={classes.top}>
+      <Grid
+        container
+        className={classes.root}
+        style={{
+          outline: 'solid blue 4px',
+          alignItems: 'center',
+          justifyContent: 'space-around',
+        }}
+      >
         <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <img
-            src={imageOne}
-            style={{ borderRadius: '.5125em', width: '40em', height: '40em' }}
-            alt='inside space'
-          />
-          <Grid container>
+          <GridList cellHeight={200} spacing={1} className={classes.gridList}>
+            <GridListTile
+              cols={2}
+              rows={2}
+              style={{ zIndex: 0, borderRadius: '5px' }}
+            >
+              <img
+                src={imageOne}
+                style={{
+                  borderRadius: '1em',
+                  //   width: '40em',
+                  //   height: '40em',
+                  //   zIndex: 0,
+                }}
+                alt='inside space'
+                position='relative'
+              />
+            </GridListTile>
+          </GridList>
+          <Grid
+            container
+            style={{
+              marginBottom: '3px',
+              display: 'flex',
+              justifyContent: 'space-around',
+              alignItems: 'center',
+            }}
+          >
             <Grid item xs={12}>
-              <Typography variant='h4' style={{ zIndex: 1 }}>
+              <Typography
+                variant='h4'
+                position='relative'
+                style={{ zIndex: 1 }}
+              >
                 Welcome to the Birdhouse
               </Typography>
             </Grid>
-            <Grid container spacing={0} className={classes.bar}>
-              <Grid item xs={false}>
-                <DatePicker
-                  disableToolbar
-                  variant='outlined'
-                  format='MM/dd/yyyy'
-                  margin='normal'
-                  id='arrival-date'
-                  label='Arrival'
-                  value={arrivalDate}
-                  onChange={handleArrivalChange}
-                  KeyboardButtonProps={{
-                    'aria-label': 'change arrival date',
-                  }}
-                />
-              </Grid>
-              <Grid item spacing={0}>
-                <DatePicker
-                  disableToolbar
-                  variant='outlined'
-                  format='MM/dd/yyyy'
-                  margin='normal'
-                  id='departure-date'
-                  label='Departure'
-                  value={departureDate}
-                  onChange={handleDepartureChange}
-                  KeyboardButtonProps={{
-                    'aria-label': 'change depature date',
-                  }}
-                />
-              </Grid>
-              <Grid item>
-                <Button
-                  endIcon={<ArrowForwardIosIcon />}
-                  variant='contained'
-                  onClick={(e) => handleBook(arrivalDate, departureDate)}
-                  className={classes.button}
-                >
-                  Book Now
-                </Button>
-              </Grid>
+            <Grid>
+              <GridList spacing={1} cols={3}>
+                {/* <Grid
+              container
+              spacing={0}
+              className={classes.bar}
+              style={{ zIndex: 2 }}
+            > */}
+                <GridListTile cols={1}>
+                  {/* <Grid item xs={false}> */}
+                  <DatePicker
+                    disableToolbar
+                    variant='outlined'
+                    format='MM/dd/yyyy'
+                    margin='normal'
+                    id='arrival-date'
+                    label='Arrival'
+                    value={arrivalDate}
+                    onChange={handleArrivalChange}
+                    KeyboardButtonProps={{
+                      'aria-label': 'change arrival date',
+                    }}
+                  />
+                </GridListTile>
+                {/* <Grid item> */}
+                <GridListTile cols={1}>
+                  <DatePicker
+                    disableToolbar
+                    variant='outlined'
+                    format='MM/dd/yyyy'
+                    margin='normal'
+                    id='departure-date'
+                    label='Departure'
+                    value={departureDate}
+                    onChange={handleDepartureChange}
+                    KeyboardButtonProps={{
+                      'aria-label': 'change depature date',
+                    }}
+                  />
+                  {/* </Grid> */}
+                </GridListTile>
+                {/* <Grid item> */}
+                <GridListTile cols={1}>
+                  <Button
+                    endIcon={<ArrowForwardIosIcon />}
+                    variant='contained'
+                    onClick={(e) => handleBook(arrivalDate, departureDate)}
+                    className={classes.button}
+                  >
+                    Book Now
+                  </Button>
+                  {/* </Grid> */}
+                </GridListTile>
+              </GridList>
             </Grid>
+            {/* </Grid> */}
+            {/* </Grid> */}
+            {dateError && <h3>Please make sure both fields have a date</h3>}
           </Grid>
-          {dateError && <h3>Please make sure both fields have a date</h3>}
         </MuiPickersUtilsProvider>
-      </Grid>
-      <Grid container spacing={2} className={classes.ui}>
-        <Grid item xs={5}>
-          <Paper elevation={0} className={classes.paper}>
-            <img src={imageOne} className={classes.image} alt='sample 1' />
-          </Paper>
-        </Grid>
-        <Grid item xs={5}>
-          <Paper elevation={0} className={classes.paper}>
-            Description
-          </Paper>
-        </Grid>
-      </Grid>
-      <Grid container spacing={2} className={classes.ui}>
-        <Grid item xs={5}>
-          <Paper elevation={0} className={classes.paper}>
-            <img src={imageOne} className={classes.image} alt='sample 2'/>
-          </Paper>
-        </Grid>
-        <Grid item xs={5}>
-          <Paper elevation={0} className={classes.paper}>
-            Description
-          </Paper>
+        <Grid
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+          }}
+        >
+          <GridList cellHeight={280} cols={2} spacing={4}>
+            <GridListTile cols={1} >
+              <img src={imageOne} className={classes.image} alt='sample 1' />
+            </GridListTile>
+            <GridListTile cols={1}>
+              <Paper elevation={0} className={classes.paper}>
+                Description
+              </Paper>
+            </GridListTile>
+          </GridList>
+          <GridList
+            cellHeight={280}
+            cols={2}
+            spacing={4}
+            // style={{ display: 'flex' }}
+          >
+            <GridListTile cols={1} rows={1}>
+              <img src={imageOne} className={classes.image} alt='sample 2' />
+            </GridListTile>
+            <GridListTile cols={1} rows={1}>
+              <Paper elevation={0} className={classes.paper}>
+                Description
+              </Paper>
+            </GridListTile>
+          </GridList>
         </Grid>
       </Grid>
     </ThemeProvider>
