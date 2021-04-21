b82a3853f2fcbeacba68df38cd4601bac63a1281
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-04-21 11:04:29 -0600

    Add theme to entire app

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,27 +11,32 @@ import Reviews from '../Reviews/Reviews'
 import Contact from '../Contact/Contact'
 import Book from '../Book/Book'
 import Header from '../Header/Header'
+import { ThemeProvider} from '@material-ui/core';
+import { theme } from '../../themes/themes';
+
 
 import './App.css';
 
 const App = () => {
   return (
-    <main className='App'>
-      <header className='App'>
-        <Header />
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/overview' component={Overview} />
-        <Route exact path='/map' component={Map} />
-        <Route exact path='/gallery' component={Gallery} />
-        <Route exact path='/rates' component={Rates} />
-        <Route exact path='/avilability' component={Availability} />
-        <Route exact path='/reviews' component={Reviews} />
-        <Route exact path='/contact' component={Contact} />
-        <Route exact path='/book' component={Book} />
-      </Switch>
-      </header>
-    </main>
+    <ThemeProvider theme={theme}>
+      <main className='App'>
+        <header className='App'>
+          <Header />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/overview' component={Overview} />
+            <Route exact path='/map' component={Map} />
+            <Route exact path='/gallery' component={Gallery} />
+            <Route exact path='/rates' component={Rates} />
+            <Route exact path='/avilability' component={Availability} />
+            <Route exact path='/reviews' component={Reviews} />
+            <Route exact path='/contact' component={Contact} />
+            <Route exact path='/book' component={Book} />
+          </Switch>
+        </header>
+      </main>
+    </ThemeProvider>
   );
 }
 
