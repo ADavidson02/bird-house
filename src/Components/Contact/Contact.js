fefe2c52bf8ac95dc2fe312efea0ce11c93615ca
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-04-23 14:04:38 -0600

    Add state for form fields

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { theme } from '../../themes/themes';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}))
 
 const Contact = () => {
-  return <h2>Contact</h2>;
+  const [name, setname ] = useState('')
+  const [email, setEmail ] = useState('')
+  const [phone, setPhone ] = useState('')
+  const [message, setMessage ] = useState('')
+
+
+  const 
+  return(
+    <h2>Contact</h2>
+  ) 
 };
 
 export default Contact;
