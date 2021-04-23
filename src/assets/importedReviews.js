21dfceb5a3479cd846a0e8f13a10e08a2ddfa91a
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-04-23 10:04:37 -0600

    Add second imported review

--- src/assets/importedReviews.js
+++ src/assets/importedReviews.js
@@ -13,5 +13,20 @@ export const importedReviews = {
                   and his wife are such super friendly people and amazing hosts.
                   Thoroughly enjoyed my stay for 2 weeks and wish it was longer`,
     },
+    {
+      id: 2,
+      name: 'Jay',
+      stars: 5,
+      writtenReview: `The bird house was a really great place for us to vacation! We
+                  stayed about a week. Could not have asked for a better place.
+                  The kitchen was stocked very well with spices and
+                  pots/pans/spatulas/plates/eating utensils. Place was sparkling
+                  clean when we arrived and responses were quick from the host.
+                  Lots of entertainment on the TV (Xbox,Netflix,Disney+). The
+                  balcony was great for eating outside and star gazing! For the
+                  price point I don’t think there’s a better place out there;
+                  highly recommend if you’re wanting a place outside the city a
+                  bit and don’t mind driving some.`,
+    },
   ],
 };
