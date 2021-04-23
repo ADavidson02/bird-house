25b837d4e0c12648791e7448356e0493d82aa8b0
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-04-23 14:16:03 -0600

    Add page Title

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,22 +1,47 @@
+
 import React, { useState } from 'react';
 import { theme } from '../../themes/themes';
+import {
+  Grid,
+  makeStyles,
+  Typography,
+  Card,
+  CardContent,
+  TextField,
+} from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
+    pageTitle: {
+    fontFamily: 'unquote',
+    fontSize: '28px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: '.5em',
+    fontWeight: 'bold',
+    textAlign: 'left',
+  },
 }))
 
 const Contact = () => {
-  const [name, setname ] = useState('')
+  const classes = useStyles();
+  const [name, setContactName ] = useState('')
   const [email, setEmail ] = useState('')
   const [phone, setPhone ] = useState('')
   const [message, setMessage ] = useState('')
+  const [error, setError] = useState('')
 
 
-  const 
+  
   return(
-    <h2>Contact</h2>
+    <div>
+      <Typography variant='h4' color='primary' className={classes.pageTitle}>
+        Contact 
+      </Typography>
+    </div>
   ) 
 };
 
