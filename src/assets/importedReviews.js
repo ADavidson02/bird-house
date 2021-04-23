97f416b1173fcf44257bf7387837027d959378b3
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-04-23 10:04:15 -0600

    Add first imported review

---
+++ src/assets/importedReviews.js
@@ -0,0 +1,17 @@
+
+export const importedReviews = {
+  reviews: [
+    {
+      id: 1,
+      name: 'Arun',
+      stars: 5,
+      writtenReview: `This property is one of the best Airbnb’s I have stayed in.
+                  Immaculate, spotless, perfection are some of the words which
+                  come to mind to describe this property. It is located in a
+                  great neighborhood and has an awesome deck. The property has
+                  all the amenities you need and more. Last but not least, Micah
+                  and his wife are such super friendly people and amazing hosts.
+                  Thoroughly enjoyed my stay for 2 weeks and wish it was longer`,
+    },
+  ],
+};
