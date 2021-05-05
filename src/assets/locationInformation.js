c07892d9d40dfd18f7be884e4f0b06224d31d7be
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-05-05 10:08:47 -0600

    Add locationInfo folder

---
+++ src/assets/locationInformation.js
@@ -0,0 +1,16 @@
+export const birdHouse = {
+  blurbs: [
+    {
+      id: 'intoBlurb',
+      name: 'Intro',
+      information: `The Bird House is a completely private studio with everything you
+                  need! With no shared spaces or walls, a large private deck with
+                  beautiful scenery, this is the perfect place to unwind after a day
+                  of exploring! Or cuddle up with the elegant electric fireplace,
+                  and watch your favorite streaming services on a 50" TV and just
+                  stay in and relax. The modern kitchen makes cooking simple &
+                  convenient and the stunning bathroom with 2 shower heads will
+                  leave you refreshed, never wanting to leave!`,
+    },
+  ],
+};
