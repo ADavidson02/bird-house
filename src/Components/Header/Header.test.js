4b7be676514de43fa76bc0d4327df0c206c15f38
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-05-13 14:17:24 -0700

    Add test for Home component lnk

---
+++ src/Components/Header/Header.test.js
@@ -0,0 +1,16 @@
+import { screen, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './Header';
+
+describe('Header', () => {
+  it('should have a link to Home', () => {
+    render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    );
+    expect(screen.getByText('Home')).toBeInTheDocument();
+  });
+});
