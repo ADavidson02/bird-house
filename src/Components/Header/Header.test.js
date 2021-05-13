8dc402c123cfaeb630ccb7028961304bd238e705
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-05-13 14:19:23 -0700

    Add test for link to overview page

--- src/Components/Header/Header.test.js
+++ src/Components/Header/Header.test.js
@@ -5,12 +5,21 @@ import { BrowserRouter } from 'react-router-dom';
 import Header from './Header';
 
 describe('Header', () => {
-  it('should have a link to Home', () => {
+  it('should have a link to home page', () => {
     render(
       <BrowserRouter>
         <Header />
       </BrowserRouter>
     );
     expect(screen.getByText('Home')).toBeInTheDocument();
+  });
+    
+  it('should have a link to the overview page', () => {
+    render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    )
+    expect(screen.getByText('Overview')).toBeInTheDocument()
   });
 });
